f27655480761e1bce0dd601f989521e88835dde5
ms-ask <ms.asck@yandex.ru>
2023-02-23 00:46:08 +0300

    feat: added client method add

--- main-service/src/test/java/ru/practicum/mainservice/MainServiceApplicationTests.java
+++ main-service/src/test/java/ru/practicum/mainservice/MainServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package ru.practicum.mainservice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.statisticclient.StatisticClient;
+
+import java.io.IOException;
+import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class MainServiceApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    @Disabled("Проверка клиента статистики. Запускается вручную, подняв сервер статистики")
+    void clientStatistic() throws IOException, InterruptedException {
+        StatisticClient client = new StatisticClient("http://localhost:9090");
+        HttpResponse<String> response = client.addHit("ewm-main-service", "/events/3",
+            "192.163.0.1", LocalDateTime.now());
+
+        Assertions.assertEquals(201, response.statusCode());
     }
 
 }
--- statistic-service/statistic-client/src/main/java/ru/practicum/statisticclient/StatisticClient.java
+++ statistic-service/statistic-client/src/main/java/ru/practicum/statisticclient/StatisticClient.java
@@ -1,2 +1,51 @@
-package ru.practicum.statisticclient;public class StatisticClient {
+package ru.practicum.statisticclient;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.stereotype.Service;
+import statisticcommon.HitRequest;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+
+@Service
+public class StatisticClient {
+
+    private final String serverUrl;
+    private final ObjectMapper jsonMapper;
+
+
+    public StatisticClient(String serverUrl) {
+        this.serverUrl = serverUrl;
+        this.jsonMapper = new ObjectMapper();
+        jsonMapper.registerModule(new JavaTimeModule());
+    }
+
+    public HttpResponse<String> addHit(String app, String uri, String ip, LocalDateTime timestamp)
+        throws IOException, InterruptedException {
+        HitRequest body = new HitRequest();
+        body.setApp(app);
+        body.setUri(uri);
+        body.setIp(ip);
+        body.setTimestamp(timestamp);
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .POST(HttpRequest.BodyPublishers.ofString(jsonMapper.writeValueAsString(body)))
+            .uri(URI.create(serverUrl + "/hit"))
+            .version(HttpClient.Version.HTTP_1_1)
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+
+        return client.send(request, handler);
+    }
 }
