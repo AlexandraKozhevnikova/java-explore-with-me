c435dbbeadcff8b16ddac133aed4cf01f95ed734
ms-ask <ms.asck@yandex.ru>
2023-02-22 16:55:41 +0300

    feat: updated  main class fo ems

--- main-service/src/main/java/ru/practicum/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/mainservice/MainServiceApplication.java
@@ -3,14 +3,20 @@ package ru.practicum.mainservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Objects;
+
 @SpringBootApplication
 public class MainServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
-        while(true){
 
-        }
+        String string;
+        do {
+            //do nothing
+            string = "some";
+
+        } while (!Objects.equals("", string));
     }
 
 }
