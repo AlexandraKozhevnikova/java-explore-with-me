87b6226f52912876d2f813d9d9f5f07c3ac63a94
ms-ask <ms.asck@yandex.ru>
2023-02-23 14:23:18 +0300

    feat: added client method get

--- main-service/src/test/java/ru/practicum/mainservice/MainServiceApplicationTests.java
+++ main-service/src/test/java/ru/practicum/mainservice/MainServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package ru.practicum.mainservice;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,22 +10,40 @@ import ru.practicum.statisticclient.StatisticClient;
 import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 class MainServiceApplicationTests {
+    private StatisticClient client;
+
+    @BeforeEach
+    void setUp() {
+        client = new StatisticClient("http://localhost:9090");
+    }
 
     @Test
     void contextLoads() {
     }
 
     @Test
-    @Disabled("Проверка клиента статистики. Запускается вручную, подняв сервер статистики")
-    void clientStatistic() throws IOException, InterruptedException {
-        StatisticClient client = new StatisticClient("http://localhost:9090");
+    @Disabled("Проверка клиента статистики POST. Запускается вручную, подняв сервер статистики")
+    void addHit_whenRequestValid_return201() throws IOException, InterruptedException {
         HttpResponse<String> response = client.addHit("ewm-main-service", "/events/3",
             "192.163.0.1", LocalDateTime.now());
 
         Assertions.assertEquals(201, response.statusCode());
     }
 
+    @Test
+    @Disabled("Проверка клиента статистики GET. Запускается вручную, подняв сервер статистики")
+    void getStatistics_whenAllParametersValid_thenReturn200() throws IOException, InterruptedException {
+        HttpResponse<String> response = client.getStatistics(
+            LocalDateTime.now().minusHours(1),
+            LocalDateTime.now(),
+            List.of("url/1", "/events/3"),
+            true
+        );
+
+        Assertions.assertEquals(200, response.statusCode());
+    }
 }
--- statistic-service/statistic-client/src/main/java/ru/practicum/statisticclient/StatisticClient.java
+++ statistic-service/statistic-client/src/main/java/ru/practicum/statisticclient/StatisticClient.java
@@ -12,18 +12,24 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 public class StatisticClient {
 
     private final String serverUrl;
     private final ObjectMapper jsonMapper;
+    private final HttpResponse.BodyHandler<String> handler;
+    private final HttpClient client;
 
 
     public StatisticClient(String serverUrl) {
         this.serverUrl = serverUrl;
         this.jsonMapper = new ObjectMapper();
         jsonMapper.registerModule(new JavaTimeModule());
+        client = HttpClient.newHttpClient();
+        handler = HttpResponse.BodyHandlers.ofString();
     }
 
     public HttpResponse<String> addHit(String app, String uri, String ip, LocalDateTime timestamp)
@@ -42,9 +48,44 @@ public class StatisticClient {
             .header("Accept", "application/json")
             .build();
 
-        HttpClient client = HttpClient.newHttpClient();
 
-        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        return client.send(request, handler);
+    }
+
+    public HttpResponse<String> getStatistics(LocalDateTime start, LocalDateTime end, List<String> uris,
+                                              Boolean unique)
+        throws IOException, InterruptedException {
+
+        if (start == null || end == null || start.isAfter(end)) {
+            throw new IllegalArgumentException("'start'  and/or 'end' not valid");
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd+HH:mm:ss");
+
+        StringBuilder uriBuilder = new StringBuilder(serverUrl + "/stats");
+        uriBuilder.append("?start=").append(start.format(formatter));
+        uriBuilder.append("&end=").append(end.format(formatter));
+
+        if (unique) {
+            uriBuilder.append("&unique=true");
+        }
+
+        if (uris != null && !uris.isEmpty()) {
+            if (uris.size() > 20) {
+                throw new IllegalArgumentException("'uris' is too much");
+            }
+            for (String uri : uris) {
+                uriBuilder.append("&uris=").append(uri);
+            }
+        }
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .GET()
+            .uri(URI.create(uriBuilder.toString()))
+            .version(HttpClient.Version.HTTP_1_1)
+            .header("Accept", "application/json")
+            .build();
+
 
         return client.send(request, handler);
     }
