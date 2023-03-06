3058e17aea5bd6a242560404f02bb295300f3531
ms-ask <ms.asck@yandex.ru>
2023-03-07 00:13:38 +0300

    feat: added compilation entity

---
+++ main-service/src/main/java/ru/practicum/main_service/controller/admin/CompilationAdminController.java
@@ -0,0 +1,4 @@
+package ru.practicum.main_service.controller.admin;
+
+public class CompilationAdminController {
+}
---
+++ main-service/src/main/java/ru/practicum/main_service/repository/CompilationRepository.java
@@ -0,0 +1,4 @@
+package ru.practicum.main_service.repository;
+
+public interface CompilationRepository {
+}
